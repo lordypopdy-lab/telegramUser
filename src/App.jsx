046b5c62f57ab0a876e258df89333a25e024faf3
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-12-09 19:35:34 +0100

    safe

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,7 @@ const App = () => {
   return (
     <div>
       <div>
-        {!connected ? (
-          <button onClick={handleConnect}>Connect to TON</button>
-        ) : (
-          <div>
-            <p>Connected: {account.address}</p>
-            <button onClick={handleDisconnect}>Disconnect</button>
-          </div>
-        )}
+       
       </div>
     </div>
   )
