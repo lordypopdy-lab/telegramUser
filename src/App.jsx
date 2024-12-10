0436dba31fed018d33ce11e253ab924e3673d84e
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-12-10 09:05:20 +0100

    safe

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,41 @@ const App = () => {
 
   const onLanguageChange = (lang) => {
     setOptions({ language: lang });
-};
+  };
 
 
-const myTransaction = {
-  validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
-  messages: [
+  const myTransaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+    messages: [
       {
-          address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
-          amount: "20000000",
-          // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
+        address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
+        amount: "20000000",
+        // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
       },
       {
-          address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
-          amount: "60000000",
-          // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
+        address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
+        amount: "60000000",
+        // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
       }
-  ]
-}
+    ]
+  }
 
   return (
     <div>
-      <TonConnectButton >TON Connect </TonConnectButton>
+      <TonConnectButton 
+      style={{float: 'right'}}
+      />
+      <div>
+        <label>language</label>
+        <select onChange={e => onLanguageChange(e.target.value)}>
+          <option value="en">en</option>
+          <option value="ru">ru</option>
+        </select>
+      </div>
       <h1>Hello World</h1>
       <button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
-                Send transaction
-            </button>
+        Send transaction
+      </button>
     </div>
   )
 }
