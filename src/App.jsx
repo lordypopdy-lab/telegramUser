06353573c8fe3529ed2dbe803bed57fc986b865e
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-12-10 09:00:41 +0100

    safe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
-import { TonConnectButton } from "@tonconnect/ui-react"
+import { TonConnectButton, Locales, useTonConnectUI } from "@tonconnect/ui-react"
 
 const App = () => {
+
+  const [tonConnectUI, setOptions] = useTonConnectUI();
+
+  const onLanguageChange = (lang) => {
+    setOptions({ language: lang });
+};
+
+
+const myTransaction = {
+  validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+  messages: [
+      {
+          address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
+          amount: "20000000",
+          // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
+      },
+      {
+          address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
+          amount: "60000000",
+          // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
+      }
+  ]
+}
+
   return (
     <div>
       <TonConnectButton >TON Connect </TonConnectButton>
