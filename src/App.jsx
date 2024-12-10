8dd5ff8114c731101656e3ac5198be311843848f
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-12-10 10:49:39 +0100

    safe

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,9 @@ const App = () => {
     validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
     messages: [
       {
-        address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
+        address: "EQDD8dqOzaj4zUK6ziJOo_G2lx6qf1TEktTRkFJ7T1c_fPQb",
         amount: "20000000",
         // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
-      },
-      {
-        address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
-        amount: "60000000",
-        // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
       }
     ]
   }
